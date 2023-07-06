c4cf784420d1c15af46f16d29611f60020c94220
David <david.ndiba@student.moringaschool.com>
2023-07-06 10:56:40 +0300

    almost done

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const search = document.querySelector('.searchbox');
 
-document.querySelector('#search-tag').onclick = () => {
-    search.classList.toggle('active')
-}
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector('#search-tag').onclick = () => {
+        search.classList.toggle('active')
+    }
+}) 
+

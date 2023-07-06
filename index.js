3432679f084133e6891b200372a1a8412bb58025
David <david.ndiba@student.moringaschool.com>
2023-07-06 15:29:56 +0300

    almost done

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-const search = document.querySelector('.searchbox');
 
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector('#search-tag').onclick = () => {
-        search.classList.toggle('active')
-    }
-}) 
+document.addEventListener("DOMContentLoaded",initialize)
+
+
+function initialize(){
+fetchData()
+
+}
+
+function fetchData(){
+fetch('http://localhost:3000/garage')
+.then(response => response.json())
+.then(data=>data.forEach(element=>{hireCar(element)}))
+
+}
+
+function hireCar(car){
+let container=document.querySelector("#garage-container")
+
+let img = document.createElement("img");
+ img.src=car.image
+ img.id=""
+container.appendChild(img)
+
+let test =document.createElement("h1")
+test.innerText=car.model
+container.appendChild(test)
+
+let year = document.createElement("p");
+year.innerText=car.year
+container.appendChild(year)
+
+let content =document.createElement("h1")
+content.innerText=`service  ${car.service_description}`
+container.appendChild(content);
+
+}
+
 

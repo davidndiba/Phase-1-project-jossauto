a14081feb7c2fd6d33073cedb52db1fb17c47f29
David <david.ndiba@student.moringaschool.com>
2023-07-06 16:07:43 +0300

    almost done

--- index.js
+++ index.js
@@ -33,7 +33,35 @@ container.appendChild(year)
 let content =document.createElement("h1")
 content.innerText=`service  ${car.service_description}`
 container.appendChild(content);
-
 }
+
+function fetchCars(){
+    fetch('http://localhost:3000/garage')
+    .then(response => response.json())
+    .then(data=>data.forEach(element=>{hireCar(element)}))
+    
+    }
+    
+    function hireCar(car){
+    let container=document.querySelector("#garage-container")
+    
+    let img = document.createElement("img");
+     img.src=car.image
+     img.id="small"
+    container.appendChild(img)
+    
+    let test =document.createElement("h1")
+    test.innerText=car.model
+    container.appendChild(test)
+    
+    let year = document.createElement("p");
+    year.innerText=car.year
+    container.appendChild(year)
+    
+    let content =document.createElement("h1")
+    content.innerText=`service  ${car.rental_price}`
+    container.appendChild(content);
+    }
+    
 
 

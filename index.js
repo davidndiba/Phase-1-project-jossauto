88a8c89dedbae4f1db1612c4bb885c24efaf417b
David <david.ndiba@student.moringaschool.com>
2023-07-09 03:11:31 +0300

    added the other two objects lists

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 
 document.addEventListener("DOMContentLoaded",initialize)
-let search = document.querySelector("search-box")
 
-document.querySelector("search-tag").onclick = () => {
-    search.classList.toggle("active");
-}
 
 function initialize(){
 fetchData()
 fetchCars()
+autoshop()
 }
 
 function fetchData(){
@@ -49,61 +46,54 @@ container.appendChild(content);
 function fetchCars(){
     fetch('http://localhost:3000/cars')
     .then(response => response.json())
-    .then((data)=>data.forEach(element=>hireCar(element)))
-    
+    .then(data=>data.forEach(element=>{hireCar(element)}))
     }
+
+    function hireCar(car) {
+        let container = document.querySelector("#car-hire");
+      
+        let image = document.createElement("img");
+        image.src = car.image;
+        container.appendChild(image);
+      
+        let model = document.createElement("h1");
+        model.innerText = car.model;
+        container.appendChild(model);
+      
+        let year = document.createElement("p");
+        year.innerText = car.year;
+        container.appendChild(year);
+      
+        let rentalPrice = document.createElement("h1");
+        rentalPrice.innerText = `Rental Price: ${car.rental_price}`;
+        container.appendChild(rentalPrice);
+      }
     
-    function hireCar(car){
-    let container=document.querySelector("#car hire")
     
-    let image = document.createElement("img");
-     image.src=car.image
-    container.appendChild(img)
-    img.innerText =`${car.model}`
-    
-    let rental_price =document.createElement("h1")
-    rental_price.innerText=car.year
-    container.appendChild(rental_price)
-    
-    let year = document.createElement("p");
-    year.innerText=car.model
-    container.appendChild(year)
-    
-    let content =document.createElement("h1")
-    content.innerText=`service  ${car.rental_price}`
-    container.appendChild(content);
-    }
 
     function autoshop(){
         fetch('http://localhost:3000/auto_shop')
         .then(response => response.json())
-        .then((data)=>data.forEach(element=>auto(element)))
+        .then((data)=>data.forEach(element=>{auto(element)}))
         
         }
         
-        function auto(car){
-        let container=document.querySelector("#cars-container")
+        function auto(spare) {
+          let container = document.querySelector("#auto-shop");
         
-        let image = document.createElement("img");
-         image.src=car.model
-        container.appendChild(img)
-        img.innerText =`${car.model}`
+          let image = document.createElement("img");
+          image.src = spare.model;
+          container.appendChild(image);
         
-        let rental_price =document.createElement("h1")
-        rental_price.innerText=car.year
-        container.appendChild(rental_price)
+          let itemPrice = document.createElement("h1");
+          itemPrice.innerText = spare.year;
+          container.appendChild(itemPrice);
         
-        let year = document.createElement("p");
-        year.innerText=car.model
-        container.appendChild(year)
+          let quantity = document.createElement("p");
+          quantity.innerText = spare.model;
+          container.appendChild(quantity);
         
-        let content =document.createElement("h1")
-        content.innerText=`service  ${car.rental_price}`
-        container.appendChild(content);
+          let description = document.createElement("h1");
+          description.innerText = `Description: ${spare.description}`;
+          container.appendChild(description);
         }
-    
-
-
-    
-
-
